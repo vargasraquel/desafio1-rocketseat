5465775c51c0c007f63bde002c201af503362794
vargasraquel <raquelvargas@gmail.com>
2021-12-06 19:51:58 -0300

    Atualizando funções com desafios

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const newTask = {
+      id: Math.round(Math.random() *100),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    if (newTaskTitle === "") {
+        alert("Insira uma tarefa.")
+    } else {
+      return setTasks([...tasks, newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+        if (task.id === id) {
+          task.isComplete = !task.isComplete;
+        }
+        return task;
+      }));
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
